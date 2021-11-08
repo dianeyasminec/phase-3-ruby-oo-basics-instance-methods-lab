501e79f6683c34927270406993ac71e785dfa496
dianeyasminec <dianeyasminec60@gmail.com>
2021-11-08 15:40:20 -0500

    commit tree 710f3e9c748f39adeee480bb99bdeea633ed1f1d

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+def bark
+    puts "Woof!"
+end
+def sitting
+    puts The Dog is sitting
+end 
+end
+
+fido = Dog.new
+fido.bark
+
+snoopy = Dog.new
+snoopy.bark
+
+sit = Dog.new
+sit.sitting
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+    def talking
+    puts "Hello World!"
+end 
+def walking
+    puts "The Person is walking"
+end 
+end
+
+talk = Person.new
+talk.talking
+
+walk = Person.new
+walk.walking
